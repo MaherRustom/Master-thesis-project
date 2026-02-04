6162ab7404bfc2f0e251a6e1889c4f43b4f8af62
Maher <maher.rustom2001@gmail.com>
2026-02-04 04:42:13 +0100

    Update chaser1.m

--- chaser1.m
+++ chaser1.m
@@ -5,16 +5,33 @@ Earth
 M_chaser1= 20 ; % [kg]
 
 %% orbital elements
-Altitude_chaser1=1000e3; %[m] altitude at perigee 
+Altitude_chaser1=36000e3; %[m] altitude at perigee 
 e_chaser1= 0.8; %[-]  Eccentricity
 i_chaser1=deg2rad(52); % [rad] Inclination
 RAAN_chaser1=deg2rad(0);% [rad] Right Ascension of Ascending Nod
 w_chaser1=deg2rad(0);% [rad] Argument of periapsis
 v_chaser1=deg2rad(0);% [rad] Tru Anomoly
+%% Attitude of  chaser, refrense fram Earth, Z north pole, y east, x outward
+rotation_chaser=[deg2rad(54),deg2rad(21),deg2rad(18)]; %[Rad] [roll,pitch, Yaw]
 
+q_chaser=[....
+cos(rotation_chaser(1)/2)*cos(rotation_chaser(2)/2)*cos(rotation_chaser(3)/2)+ sin(rotation_chaser(1)/2)*sin(rotation_chaser(2)/2)*sin(rotation_chaser(3)/2);
+sin(rotation_chaser(1)/2)*cos(rotation_chaser(2)/2)*cos(rotation_chaser(3)/2)- cos(rotation_chaser(1)/2)*sin(rotation_chaser(2)/2)*sin(rotation_chaser(3)/2);
+cos(rotation_chaser(1)/2)*sin(rotation_chaser(2)/2)*cos(rotation_chaser(3)/2)+ sin(rotation_chaser(1)/2)*cos(rotation_chaser(2)/2)*sin(rotation_chaser(3)/2);
+cos(rotation_chaser(1)/2)*cos(rotation_chaser(2)/2)*sin(rotation_chaser(3)/2)- sin(rotation_chaser(1)/2)*sin(rotation_chaser(2)/2)*cos(rotation_chaser(3)/2);
+];
+
+q_chaser=q_chaser/norm(q_chaser);
+
+% wikipedia is the source: quaternion to Euler angel
+rotation_chaser(1)=atan2(2*( q_chaser(1)*q_chaser(2)+q_chaser(3)*q_chaser(4)),  1-2*(q_chaser(2)^2+q_chaser(3)^2));
+rotation_chaser(2)=-pi/2+2*atan2(  sqrt(1+2*( q_chaser(1)*q_chaser(3)-q_chaser(2)*q_chaser(4)))   , sqrt(1-2*( q_chaser(1)*q_chaser(3)-q_chaser(2)*q_chaser(4)))   );
+rotation_chaser(3)=atan2(2*( q_chaser(1)*q_chaser(4)+q_chaser(2)*q_chaser(3)),  1-2*(q_chaser(3)^2+q_chaser(4)^2));
+
+angular_rotation_chaser1=[0,0,0]; % [rad/s] [wx,wy,wz]
 %%
 a_chaser1= (Altitude_chaser1+R_Earth)/(1-e_chaser1); % [m] semi major axis
-T_chaser1=2*pi*sqrt(a_chaser1^3/mu_Earth); %[S] orbirt period
+T_chaser1=2*pi*sqrt(a_chaser1^3/mu_Earth); %[s] orbirt period
 tspan=linspace(0,1*T_chaser1,1000);
 %%
 R_chaser1= a_chaser1*(1-e_chaser1^2)/(1+e_chaser1*cos(v_chaser1));% position chaser
